6e0b8a9f25c21a2021334a7715f8b7e8533083e9
Stela Capsa <stela.capsa@gmail.com>
2020-02-21 15:18:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ end
    
    def self.students_below_12th_grade
      sql = <<-SQL
-     SELECT * FROM students WHERE grade =< 11
+     SELECT * FROM students WHERE grade < 12
      SQL
      
      DB[:conn].execute(sql)
