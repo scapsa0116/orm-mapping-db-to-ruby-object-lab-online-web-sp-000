f17e4ec6925f9c1c39607e74ef992a21feb69912
Stela Capsa <stela.capsa@gmail.com>
2020-02-21 15:40:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,8 @@ class Student
 
   def self.all
     sql = <<-SQL
-    SELECT * FROM students
+    SELECT * 
+    FROM students
     SQL
     
     DB[:conn].execute(sql).map do |row|
@@ -22,7 +23,9 @@ end
 
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT * FROM students WHERE name = ?
+    SELECT * 
+    FROM students
+    WHERE name = ?
     LIMIT 1
     
     SQL
@@ -34,7 +37,9 @@ end
    
    def self.all_students_in_grade_9
      sql = <<-SQL
-     SELECT * FROM students WHERE grade = 9
+     SELECT * 
+     FROM students 
+     WHERE grade = 9
      SQL
      
      DB[:conn].execute(sql)
@@ -53,7 +58,10 @@ end
    
   def self.first_x_students_in_grade_10(x)
      sql = <<-SQL
-    SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SELECT * 
+    FROM students 
+    WHERE grade = 10 
+    LIMIT ?
     SQL
     DB[:conn].execute(sql, x)[0]
     
@@ -61,7 +69,9 @@ end
   
   def self.first_student_in_grade_10
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = 10 
+    SELECT * 
+    FROM students 
+    WHERE grade = 10 
     SQL
     
     firts_student = DB[:conn].execute(row)[0]
