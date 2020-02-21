95ddc9ab775c424527dab7b72e2938f8b86704e8
Stela Capsa <stela.capsa@gmail.com>
2020-02-21 15:35:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ end
      sql = <<-SQL
      SELECT * FROM students WHERE grade < 12
      SQL
-     DB[:conn].execute(sql,name)
+     DB[:conn].execute(sql)
    end 
    
   def self.first_x_students_in_grade_10(x)
