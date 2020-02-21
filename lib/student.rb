7fae52f537f9838adabecd0b49cc4f325fbcecac
Stela Capsa <stela.capsa@gmail.com>
2020-02-21 16:31:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ end
   end
    
  def self.first_x_students_in_grade_10(x)
-    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"	    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+  sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
 	 DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
