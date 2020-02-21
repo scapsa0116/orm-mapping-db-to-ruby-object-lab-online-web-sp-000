86fee2480ca2ee4dcbb3a638bca83c009d2c1f43
Stela Capsa <stela.capsa@gmail.com>
2020-02-21 16:04:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ end
     WHERE students.grade = 10 
     LIMIT ?
     SQL
-    DB[:conn].execute(sql, x) #.map do |row|
+    s = DB[:conn].execute(sql, x) #.map do |row|
     #self.new_from_db(row)
     end 
   
